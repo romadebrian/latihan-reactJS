aad6eedc306f6e4dbe8d8757ffc681b848b1bd7d
romadebrian <roma_coll04@yahoo.com>
2021-10-25 16:49:14 +0700

    LifeCycleComponent

---
+++ ReactTutorial/my-app/src/countainer/LifeCycleComp/LifeCycleComp.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class LifeCycleComp extends Component {
+  render() {
+    return <button>Component Button</button>;
+  }
+}
+
+export default LifeCycleComp;
