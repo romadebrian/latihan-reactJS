2197eb829e47a79fbd638e83d494dae4c74946a4
romadebrian <roma_coll04@yahoo.com>
2021-10-25 18:30:21 +0700

    Functrion per LifeCryle

--- ReactTutorial/my-app/src/countainer/LifeCycleComp/LifeCycleComp.jsx
+++ ReactTutorial/my-app/src/countainer/LifeCycleComp/LifeCycleComp.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import "./LifeCycleComp.css";
 
 class LifeCycleComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+  }
+
+  static getDerivedStateFormProps(props, state) {
+    console.log("getDerivedStateFormProps");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
   render() {
-    return <button>Component Button</button>;
+    console.log("render");
+    return <button className="btn">Component Button</button>;
   }
 }
 
