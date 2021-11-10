661078fda1f5579048b1ea965299eb6c90fcd0ab
romadebrian <roma_coll04@yahoo.com>
2021-11-10 13:52:22 +0700

    Json Dummy Server Data

--- ReactTutorial/my-app/src/countainer/BlogPost/BlogPost.jsx
+++ ReactTutorial/my-app/src/countainer/BlogPost/BlogPost.jsx
@@ -17,7 +17,7 @@ class BlogPost extends Component {
     //     });
     //   });
 
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((result) => {
+    axios.get("http://localhost:3004/posts").then((result) => {
       this.setState({
         post: result.data,
       });
