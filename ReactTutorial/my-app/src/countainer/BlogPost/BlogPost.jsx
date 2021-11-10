c2f39107de1f0a57072e78958edfb26869071be0
romadebrian <roma_coll04@yahoo.com>
2021-11-10 12:44:31 +0700

    import axios

--- ReactTutorial/my-app/src/countainer/BlogPost/BlogPost.jsx
+++ ReactTutorial/my-app/src/countainer/BlogPost/BlogPost.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react"; //rcc
 import Post from "../../component/Post/Post";
 import "./BlogPost.css";
+import axios from "axios";
 
 class BlogPost extends Component {
   state = {
@@ -11,17 +12,19 @@ class BlogPost extends Component {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
-          this.setState({
-              post: json
-          })
-      }
+        this.setState({
+          post: json,
+        });
+      });
   }
 
   render() {
     return (
       <Fragment>
         <h2 className="section-title">Blog Post</h2>
-        <Post title="Title" desc="Description" />
+        {this.state.post.map((post) => {
+          return <Post key={post.id} title={post.title} desc={post.body} />;
+        })}
       </Fragment>
     );
   }
