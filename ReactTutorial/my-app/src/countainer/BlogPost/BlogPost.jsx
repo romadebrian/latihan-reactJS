0c14e2df89364fb2ed9294b7b81143f52789051b
romadebrian <roma_coll04@yahoo.com>
2021-11-10 11:21:59 +0700

    BlogPost

---
+++ ReactTutorial/my-app/src/countainer/BlogPost/BlogPost.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react"; //rcc
+
+export default class BlogPost extends Component {
+  render() {
+    return <div> Blog Post</div>;
+  }
+}
+
+export default BlogPost;
