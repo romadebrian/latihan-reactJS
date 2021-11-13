7bd73332eafd4ee1dc1fc2a8dee10cb228b96a2e
romadebrian <roma_coll04@yahoo.com>
2021-11-13 22:41:28 +0700

    Styling Detail Post (CSS)

--- ReactTutorial/my-app/src/countainer/pages/BlogPost/DetailPost/DetailPost.jsx
+++ ReactTutorial/my-app/src/countainer/pages/BlogPost/DetailPost/DetailPost.jsx
@@ -1,13 +1,34 @@
+import Axios from "axios";
 import React, { Component } from "react";
+import "./DetailPost.css";
 
 class DetailPost extends Component {
+  state = {
+    post: {
+      title: "",
+      body: "",
+    },
+  };
+
   componentDidMount() {
-    console.log(this.props.match.params.PostId);
+    // console.log(this.props.match.params.PostId);
+    let id = this.props.match.params.PostId;
+    Axios.get(`http://localhost:3004/posts/${id}`).then((res) => {
+      console.log("result: ", res);
+      let post = res.data; //untuk memperpendek
+      this.setState({
+        post: {
+          title: post.title,
+          body: post.body,
+        },
+      });
+    });
   }
   render() {
     return (
-      <div>
-        <h1> DetailPost</h1>
+      <div className="p-detail-post">
+        <p className="detail-title">{this.state.post.title}</p>
+        <p className="detail-body">{this.state.post.body}</p>
       </div>
     );
   }
