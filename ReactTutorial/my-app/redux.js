f612438d0d4b9aa86440c311230e1ff341c54056
romadebrian <roma_coll04@yahoo.com>
2021-11-14 12:37:23 +0700

    redux subcribe function

--- ReactTutorial/my-app/redux.js
+++ ReactTutorial/my-app/redux.js
@@ -5,21 +5,46 @@ const initialState = { value: 0, age: 17 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-  if (action.type === "ADD_AGE") {
-    return {
-      ...state,
-      age: state.age + 1,
-    };
+  //   if (action.type === "ADD_AGE") {
+  //     return {
+  //       ...state,
+  //       age: state.age + 1,
+  //     };
+  //   }
+  //   if (action.type === "CHANGE_VALUE") {
+  //     return {
+  //       ...state,
+  //       value: state.value + action.newValue,
+  //     };
+  //   }
+  //   return state;
+  switch (action.type) {
+    case "ADD_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    case "CHANGE_VALUE":
+      return {
+        ...state,
+        value: state.value + action.newValue,
+      };
+
+    default:
+      return state;
   }
-  return state;
 };
 
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+    console.log('store change: ', store.getState())
+})
+
 // Dispacting Action
 store.dispatch({ type: "ADD_AGE" });
+store.dispatch({ type: "CHANGE_VALUE", newValue: 12 });
 console.log(store.getState());
-
-// Subscription
