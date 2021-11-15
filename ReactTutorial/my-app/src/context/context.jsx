c70047530a8d1d0ac558a4db8111b3597e2a58e0
romadebrian <roma_coll04@yahoo.com>
2021-11-15 17:30:25 +0700

    Spare to another file and make it more structure

---
+++ ReactTutorial/my-app/src/context/context.jsx
@@ -0,0 +1,44 @@
+import React, { Component, createContext } from "react";
+
+// const Provider = createContext.Provider;
+const RootContext = createContext();
+const Provider = RootContext.Provider;
+
+// Provider
+const GlobalProvider = (Childern) => {
+  return class ParentComp extends Component {
+    state = {
+      totalOrder: 55,
+    };
+
+    dispatch = (action) => {
+      if (action.type === "PLUS_ORDER") {
+        return this.setState({
+          totalOrder: this.state.totalOrder + 1,
+        });
+      }
+      if (action.type === "MINUS_ORDER") {
+        return this.setState({
+          totalOrder: this.state.totalOrder - 1,
+        });
+      }
+    };
+
+    render() {
+      return (
+        <Provider
+          value={{
+            state: this.state,
+            dispatch: this.dispatch,
+          }}
+        >
+          <Childern {...this.props} />
+        </Provider>
+      );
+    }
+  };
+};
+
+export default GlobalProvider;
+
+// Cosumer
