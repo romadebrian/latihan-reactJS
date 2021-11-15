42a4a32cfd6ca4a41e5072d65be1f58c86502c15
romadebrian <roma_coll04@yahoo.com>
2021-11-15 17:30:41 +0700

    move fuction of context

--- ReactTutorial/my-app/src/countainer/Home/Home.jsx
+++ ReactTutorial/my-app/src/countainer/Home/Home.jsx
@@ -12,10 +12,6 @@ import YoutubeCompPage from "../pages/YoutubeCompPage/YoutubeCompPage";
 //Style
 import "./Home.css";
 
-// const Provider = createContext.Provider;
-export const RootContext = createContext();
-const Provider = RootContext.Provider;
-
 class Home extends Component {
   state = {
     showComponent: true,
@@ -29,50 +25,26 @@ class Home extends Component {
     // }, 10000);
   }
 
-  state = {
-    totalOrder: 55,
-  };
-
-  dispatch = (action) => {
-    if (action.type === "PLUS_ORDER") {
-      return this.setState({
-        totalOrder: this.state.totalOrder + 1,
-      });
-    }
-    if (action.type === "MINUS_ORDER") {
-      return this.setState({
-        totalOrder: this.state.totalOrder - 1,
-      });
-    }
-  };
-
   render() {
     return (
       <Router>
-        <Provider
-          value={{
-            state: this.state,
-            dispatch: this.dispatch,
-          }}
-        >
-          <Fragment>
-            <div className="navigation">
-              <Link to="/">Blog Post</Link>
-              <Link to="/product">Product</Link>
-              <Link to="/lifecycle">LifeCycle</Link>
-              <Link to="/youtube-component">Youtube</Link>
-            </div>
+        <Fragment>
+          <div className="navigation">
+            <Link to="/">Blog Post</Link>
+            <Link to="/product">Product</Link>
+            <Link to="/lifecycle">LifeCycle</Link>
+            <Link to="/youtube-component">Youtube</Link>
+          </div>
 
-            <Route path="/" exact component={BlogPost} />
-            <Route path="/detail-post/:PostId" component={DetailPost} />
-            <Route path="/product" component={Product} />
-            <Route path="/lifecycle" component={LifeCycleComp} />
-            <Route path="/youtube-component" component={YoutubeCompPage} />
-          </Fragment>
-        </Provider>
+          <Route path="/" exact component={BlogPost} />
+          <Route path="/detail-post/:PostId" component={DetailPost} />
+          <Route path="/product" component={Product} />
+          <Route path="/lifecycle" component={LifeCycleComp} />
+          <Route path="/youtube-component" component={YoutubeCompPage} />
+        </Fragment>
       </Router>
     );
   }
 }
 
-export default Home;
+export default GlobalProvider(Home);
