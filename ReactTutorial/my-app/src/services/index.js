064ab9e8e589feb6228287e0b01ed6cea0676181
romadebrian <roma_coll04@yahoo.com>
2021-11-16 18:06:55 +0700

    Split Function Get and Post to New File

--- ReactTutorial/my-app/src/services/index.js
+++ ReactTutorial/my-app/src/services/index.js
@@ -1,36 +1,5 @@
-import axios from "axios";
-
-const RootPath = "http://localhost:3004";
-const OnlineRoot = "https://jsonplaceholder.typicode.com";
-
-const Get = (path, root) => {
-  const promise = new Promise((resolve, reject) => {
-    axios.get(`${root ? OnlineRoot : RootPath}/${path}`).then(
-      (result) => {
-        resolve(result.data);
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-  return promise;
-};
-
-const Post = (path, root, data) => {
-  const promise = new Promise((resolve, reject) => {
-    axios.post(`${root ? OnlineRoot : RootPath}/${path}`, data).then(
-      (result) => {
-        resolve(result);
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-
-  return promise;
-};
+import Get from "./Get";
+import Post from "./Post";
 
 // POST
 const postNewsBlog = (data) => Post(`posts`, false, data);
