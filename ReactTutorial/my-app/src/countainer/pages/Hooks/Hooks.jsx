fb6f52e7d0f6a55271127f9a879ef1be95056721
romadebrian <roma_coll04@yahoo.com>
2021-11-16 21:47:41 +0700

    Styling page hooks

--- ReactTutorial/my-app/src/countainer/pages/Hooks/Hooks.jsx
+++ ReactTutorial/my-app/src/countainer/pages/Hooks/Hooks.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "./Hooks.css";
 
 class Hooks extends Component {
   render() {
     return (
-      <div>
+      <div className="p-hooks">
         <p>Nilai Saya saat ini adalah : 0</p>
         <button>Update Nilai</button>
       </div>
