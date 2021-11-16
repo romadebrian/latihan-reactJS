747073a42a6af120d3457e7d0ea781f150199ff8
romadebrian <roma_coll04@yahoo.com>
2021-11-16 21:39:18 +0700

    Base Front End Hooks

---
+++ ReactTutorial/my-app/src/countainer/pages/Hooks/Hooks.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Hooks extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nilai Saya saat ini adalah : 0</p>
+        <button>Update Nilai</button>
+      </div>
+    );
+  }
+}
+
+export default Hooks;
