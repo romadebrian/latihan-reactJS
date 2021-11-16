d5c55a89cbc2eb8e7f47e21178958994e1abf73d
romadebrian <roma_coll04@yahoo.com>
2021-11-16 18:16:19 +0700

    Add Action Put

--- ReactTutorial/my-app/src/services/index.js
+++ ReactTutorial/my-app/src/services/index.js
@@ -1,8 +1,12 @@
 import Get from "./Get";
 import Post from "./Post";
+import Put from "./Put";
 
 // POST
 const postNewsBlog = (data) => Post(`posts`, false, data);
+
+// PUT
+const updateNewsBlog = (data, id) => Post(`posts/${id}`, false, data);
 
 // GET
 const getNewsBlog = () => Get("posts?_sort=id&_order=desc", false);
