b39c5a56f15130fc3f020c6ad4ee60505acd41b8
romadebrian <roma_coll04@yahoo.com>
2021-11-16 22:13:55 +0700

    Change title page

--- ReactTutorial/my-app/src/countainer/pages/Hooks/Hooks.jsx
+++ ReactTutorial/my-app/src/countainer/pages/Hooks/Hooks.jsx
@@ -1,34 +1,39 @@
 import React, { Component, useState } from "react";
 import "./Hooks.css";
 
-// class Hooks extends Component {
-//   state = {
-//     count: 0,
-//   };
+class Hooks extends Component {
+  state = {
+    count: 0,
+  };
 
-//   onUpdate = () => {
-//     this.setState({
-//       count: this.state.count + 1,
-//     });
-//   };
-//   render() {
-//     return (
-//       <div className="p-hooks">
-//         <p>Nilai Saya saat ini adalah : {this.state.count}</p>
-//         <button onClick={this.onUpdate}>Update Nilai</button>
-//       </div>
-//     );
-//   }
-// }
+  onUpdate = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 
-const Hooks = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div className="p-hooks">
-      <p>Nilai Saya saat ini adalah : {count}</p>
-      <button onClick={() => setCount(count + 1)}>Update Nilai</button>
-    </div>
-  );
-};
+  componentDidMount() {
+    document.title = `Title Change: ${this.state.count}`;
+  }
+
+  render() {
+    return (
+      <div className="p-hooks">
+        <p>Nilai Saya saat ini adalah : {this.state.count}</p>
+        <button onClick={this.onUpdate}>Update Nilai</button>
+      </div>
+    );
+  }
+}
+
+// const Hooks = () => {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div className="p-hooks">
+//       <p>Nilai Saya saat ini adalah : {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Update Nilai</button>
+//     </div>
+//   );
+// };
 
 export default Hooks;
