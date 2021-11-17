ace9a07d119ecb273e79c5130aa23a713115cca3
romadebrian <roma_coll04@yahoo.com>
2021-11-17 16:16:47 +0700

    Base Page

---
+++ ReactTutorial/simple-notes-firebase/src/containers/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <p>Page Register</p>
+      </div>
+    );
+  }
+}
+
+export default Register;
