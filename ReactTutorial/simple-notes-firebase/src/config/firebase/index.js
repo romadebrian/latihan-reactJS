70c32d92d121a2278cfeda4053d249c061c61654
romadebrian <roma_coll04@yahoo.com>
2021-11-17 14:40:28 +0700

    import firebase and install firebase

---
+++ ReactTutorial/simple-notes-firebase/src/config/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBl3ImJzmZ6ynszSa5ku6SlgmQpU11tIn4",
+  authDomain: "simple-notes-firebase-a134f.firebaseapp.com",
+  projectId: "simple-notes-firebase-a134f",
+  storageBucket: "simple-notes-firebase-a134f.appspot.com",
+  messagingSenderId: "701779140103",
+  appId: "1:701779140103:web:f1f981ee687687adfe54a2",
+  measurementId: "G-16HN3VD5PE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
