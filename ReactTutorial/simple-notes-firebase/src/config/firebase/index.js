673f6a201f0cfd320980d525fa3d4fe87ae14b10
romadebrian <roma_coll04@yahoo.com>
2021-11-17 14:43:28 +0700

    export firebase

--- ReactTutorial/simple-notes-firebase/src/config/firebase/index.js
+++ ReactTutorial/simple-notes-firebase/src/config/firebase/index.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+// import "firebase/auth";
+// import "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default firebase;
