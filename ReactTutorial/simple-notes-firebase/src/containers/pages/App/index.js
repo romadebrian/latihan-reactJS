83f373e3fc4ea2c0b5fcaef38e8ab8b5e23cf14c
romadebrian <roma_coll04@yahoo.com>
2021-11-18 23:40:38 +0700

    setup file & folder for redux

--- ReactTutorial/simple-notes-firebase/src/containers/pages/App/index.js
+++ ReactTutorial/simple-notes-firebase/src/containers/pages/App/index.js
@@ -1,35 +1,11 @@
 import React from "react";
-// import logo from "../../../assets/img/logo/logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "../Dashboard";
 import Login from "../Login";
 import Register from "../Register";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-
-const initialState = {
-  popup: "false",
-  isLogin: false,
-};
-
-const reducer = (state = initialState, action) => {
-  if (action.type === "CHANGE_POPUP") {
-    return {
-      ...state,
-      popup: action.value,
-    };
-  }
-  if (action.type === "CHANGE_ISLOGIN") {
-    return {
-      ...state,
-      isLogin: action.value,
-    };
-  }
-  return state;
-};
-
-const store = createStore(reducer);
+import { store } from "../../../config/redux";
 
 function App() {
   return (
