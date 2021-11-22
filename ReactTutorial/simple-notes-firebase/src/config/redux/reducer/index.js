73432386e75c85cf888c9884c68473da2230ce50
romadebrian <roma_coll04@yahoo.com>
2021-11-22 18:47:27 +0700

    add action SET_NOTES in reducer

--- ReactTutorial/simple-notes-firebase/src/config/redux/reducer/index.js
+++ ReactTutorial/simple-notes-firebase/src/config/redux/reducer/index.js
@@ -2,7 +2,8 @@ const initialState = {
   popup: false,
   isLogin: false,
   isLoading: false,
-  user: "Hudoro",
+  user: {},
+  notes: "",
 };
 
 const reducer = (state = initialState, action) => {
@@ -28,6 +29,12 @@ const reducer = (state = initialState, action) => {
     return {
       ...state,
       isLoading: action.value,
+    };
+  }
+  if (action.type === "SET_NOTES") {
+    return {
+      ...state,
+      notes: action.value,
     };
   }
   return state;
